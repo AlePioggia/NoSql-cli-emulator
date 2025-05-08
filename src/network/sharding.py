bb5dfd25409b351ebd7a7932efdeb28d4b6fbea3
Alessandro Pioggia <alessandro.pioggia2@studio.unibo.it>
2025-05-08 10:18:12 +0200

    chore: remove typo

--- src/network/sharding.py
+++ src/network/sharding.py
@@ -8,10 +8,8 @@ class ShardingManager:
 
     def getHashedShardNumber(self, key: str, nodes_number:int = None) -> int:
         if nodes_number is None:
-            # nodes_number = settings.Settings.PEERS_NUMBER
             nodes_number = len(os.getenv("ALL_PEERS", "").split(","))
         
         key_hash = xxhash.xxh64(key.encode()).intdigest()
         destination_node = key_hash % nodes_number
         return destination_node
-
