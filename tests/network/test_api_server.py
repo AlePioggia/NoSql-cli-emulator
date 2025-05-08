d6eca028d5d650fffd535c9039165bbdd1d33292
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2025-05-08 15:56:02 +0200

    test: add api server new test

--- tests/network/test_api_server.py
+++ tests/network/test_api_server.py
@@ -1,32 +1,57 @@
-# import pytest
-# from httpx import AsyncClient, ASGITransport
-# from main import app
+import pytest
+from fastapi.testclient import TestClient
+from src.network.api_server import app
+from types import SimpleNamespace
 
-# @pytest.fixture(autouse=True)
-# async def setup_and_teardown():
-#     await app.router.startup()
-#     yield
-#     await app.router.shutdown()
+class ProxyStore:
+    def __init__(self):
+        self.data = {}
+        self.vc = {}
 
-# @pytest.mark.asyncio
-# async def test_set_key():
-#     async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
-#         response = await ac.post("/set/foo", json={"value": "bar"})
-#     assert response.status_code == 200
-#     assert response.json() == {"key": "foo", "value": "bar"}
+    async def get(self, key):
+        return self.data.get(key)
 
-# @pytest.mark.asyncio
-# async def test_get_key():
-#     async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
-#         await ac.post("/set/foo", json={"value": "bar"})
-#         response = await ac.get("/get/foo")
-#     assert response.status_code == 200
-#     assert response.json() == {"key": "foo", "value": "bar"}
+    async def set(self, key, value, vc):
+        self.data[key] = value
+        self.vc[key] = vc
 
-# @pytest.mark.asyncio
-# async def test_delete_key():
-#     async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as ac:
-#         await ac.post("/set/foo", json={"value": "bar"})
-#         response = await ac.delete("/delete/foo")
-#     assert response.status_code == 200
-#     assert response.json() == {"key": "foo"}
+    async def getVectorClock(self, key):
+        return self.vc.get(key)
+
+    async def delete(self, key):
+        if key in self.data:
+            del self.data[key]
+
+    async def keys(self):
+        return self.data.keys()
+
+class ProxyGossip:
+    async def add_update(self, update): pass
+
+@pytest.fixture
+def client():
+    app.state.store = ProxyStore()
+    app.state.gossip_manager = ProxyGossip()
+
+    return TestClient(app)
+
+def test_set_get_delete(client):
+    headers = {"X-API-KEY": "abcd1234-admin-2099-12-31"}
+
+    res = client.post('/set/foo', headers=headers, json={"value": "bar"})
+    assert res.status_code == 200
+    assert res.json() == {"key": "foo", "value": "bar"}
+
+    res = client.get('/get/foo', headers=headers)
+    assert res.status_code == 200
+    assert res.json() == {"key": "foo", "value": "bar"}
+
+    res = client.delete('/delete/foo', headers=headers)
+    assert res.status_code == 200
+    assert res.json() == {"key": "foo"}
+
+def test_get_non_existent_key(client):
+    headers = {"X-API-KEY": "abcd1234-admin-2099-12-31"}
+    res = client.get('/get/non_existent_key', headers=headers)
+    assert res.status_code == 404
+    assert res.json() == {"detail": "Key not found"}
